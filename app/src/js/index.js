f4f5d78a2a67483e0672d016bd0624ed36d4e3f2
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-06-03 15:35:29 +0900

    server-side-renderingに伴いroutesを別ファイル化して共通で使用するように

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/App'
-import About from './components/About'
-import ArticleList from './components/ArticleList'
-import Article from './components/Article'
+import routes from './routes.js'
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="about" component={About} />
-      <Route path="articles" component={ArticleList} />
-      <Route path="articles/:id" component={Article} />
-    </Route>
-    <Route path="*" component={App}/>
-  </Router>
+  <Router routes={routes} history={browserHistory} />
 ), document.getElementById('content'))
